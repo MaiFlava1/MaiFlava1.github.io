a5c5dd2d13ba6781a29b6977a9d658068032c4c9
MaiFlava <maiflava1@gmail.com>
2025-05-23 03:26:51 +0900

    update

--- js/utility.js
+++ js/utility.js
@@ -1,5 +1,22 @@
+closeCustomAlert();
+
+function showCustomAlert(title, message) {
+    document.getElementById('alertTitle').innerText = title;
+    document.getElementById('alertMessage').innerText = message;
+    document.getElementById('customAlert').style.display = 'flex';
+}
+
+function closeCustomAlert() {
+    document.getElementById('customAlert').style.display = 'none';
+}
+
 function copyEmail() {
-  navigator.clipboard.writeText('MaiFlava1@gmail.com')
-    .then(() => alert('MaiFlava1@gmail.com\nEmail copied to clipboard'))
-    .catch(err => console.error('MaiFlava1@gmail.com\nClipboard copy failed:', err));
+    navigator.clipboard.writeText('MaiFlava1@gmail.com')
+        .then(() => {
+            showCustomAlert('Email Copied!', 'MaiFlava1@gmail.com\nEmail has been copied to your clipboard.');
+        })
+        .catch(err => {
+            console.error('MaiFlava1@gmail.com\nClipboard copy failed:', err);
+            showCustomAlert('Error', 'Failed to copy email: ' + err.message);
+        });
 }
